97e2c850d572562159ae97e92a9f3f8905a85e44
Lili Cosic <lili@kinvolk.io>
2016-12-02 13:36:33 +0000

    fix schema build

--- extension.mk
+++ extension.mk
@@ -1,6 +1,6 @@
 include $(top_srcdir)/include.mk
 
 dist_extension_DATA = extension.js stylesheet.css metadata.json $(EXTRA_MODULES)
-nodist_extension_DATA = metadata.json $(top_srcdir)/lib/convenience.js $(EXTRA_EXTENSION)
+#nodist_extension_DATA = metadata.json $(top_srcdir)/lib/convenience.js $(EXTRA_EXTENSION)
 
 
