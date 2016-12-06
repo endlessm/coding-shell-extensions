60d67adc58c834bcea015cd1684cfb9c254453e8
Lili Cosic <lili@kinvolk.io>
2016-12-06 11:16:34 +0000

    change dest dir

--- include.mk
+++ include.mk
@@ -2,11 +2,10 @@ extensionurl = http://git.gnome.org/gnome-shell-extensions
 
 # Change these to modify how installation is performed
 topextensiondir = $(datadir)/gnome-shell/extensions
-extensionbase = @gnome-shell-extensions.gcampax.github.com
 
 gschemabase = org.gnome.shell.extensions
 
 uuid = $(EXTENSION_ID)
-gschemaname = $(gschemabase).$(EXTENSION_ID)
+gschemaname = $(gschemabase).$(SCHEMA_ID)
 
 extensiondir = $(topextensiondir)/$(uuid)
