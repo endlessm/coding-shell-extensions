b62eab044b35ddc7ca60a89a247702d6a12a6912
Simon Schampijer <simon.schampijer@endlessm.com>
2016-12-15 22:56:51 +0100

    Cleanup of build instructions and debian packaging

--- autogen.sh
+++ autogen.sh
@@ -3,8 +3,6 @@
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
-PKG_NAME="coding-shell-extensions"
-
 test -f $srcdir/configure.ac || {
     echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
     echo " top-level $(PKG_NAME) directory"
